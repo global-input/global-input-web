0d85ff2e0df8841a32e97c42b93a8d4a0e1f442f
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-04 00:56:18 +0100

    initial import

---
+++ deploy/deploy.sh
@@ -0,0 +1,12 @@
+export deploy_to_hostname="$1"
+export deploy_to_username="$2"
+export projectversion="$3"
+
+source deploy/util.sh
+buildVariables
+createFolders
+uploadZipFile
+unzipZipFile
+makeSchellScriptExecutable
+copyTheAppToDockerFolder
+buildAndStartDocker
