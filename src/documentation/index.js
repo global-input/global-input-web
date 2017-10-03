539d90df10937f92af74f98914fcdec68ec000ed
Dilshat Hewzulla <hewzulla@gmail.com>
2017-10-04 00:05:01 +0100

    releasing

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -23,7 +23,7 @@ render() {
             <div className="container" id="mainContent">
                 <DisplayBlockText title={textValues.documentation.getStarted.title} content={textValues.documentation.getStarted.content}/>
                 <DisplayBlockText title={textValues.documentation.installation.title} content={textValues.documentation.installation.content}/>
-                
+
             </div>
             <ContactUsButton/>
       </div>
