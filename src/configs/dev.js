8de486945f5b01a634183217c345c032b008a73e
Dilshat Hewzulla <hewzulla@gmail.com>
2018-03-22 09:50:56 +0000

    wrong links is fixed

--- src/configs/dev.js
+++ src/configs/dev.js
@@ -1,6 +1,8 @@
 var config={
-    apikey:"AME6hDRWzGLfCfZbs",
-    url:"http://192.168.0.5:1337",
-    securityGroup:"localtest"
+
+    url:"http://192.168.0.6:1337",
+    apikey:"k7jc3QcMPKEXGW5UC",
+    securityGroup:"1CNbWCFpsbmRQuKdd"
+
 }
 export default config;
