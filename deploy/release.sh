7ad18098dd42a05433d6b5275e49efb23fd11d18
Dilshat Hewzulla <hewzulla@gmail.com>
2018-06-02 22:57:23 +0100

    releasing

--- deploy/release.sh
+++ deploy/release.sh
@@ -3,8 +3,8 @@ source deploy/util.sh
 git add .
 git commit -m "releasing"
 yarn build
-mvn jgitflow:release-start
-mvn jgitflow:release-finish
+mvn gitflow:release-start
+mvn gitflow:release-finish
 git checkout  master
 getProjectVersionFromPom
 git checkout develop
