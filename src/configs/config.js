81014f64cebaaf5e4e8866b7ad617dffaeabb7d0
Dilshat Hewzulla <hewzulla@gmail.com>
2018-09-12 12:01:29 +0100

    video

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,9 +6,9 @@ config.videos={
    videoPlayer:function(){return this.location+"/glbal_input_media_player.mp4";},
    sendMessage:function(){return this.location+"/globbal_input_send_message.mp4";},
    formTransfer:function(){return this.location+"/global_input_transfer_form_2.mp4";},
-   signin:function(){return this.location+"/global_input_sign_in.mp4";},
+   signin:function(){return this.location+"/global_input_sign_in_2.mp4";},
    copyAndPaste:function(){return this.base+"/copy-and-paste.mp4";},
-   qrPrinting:function(){return this.base+"/qr-printing.mp4";},
+   qrPrinting:function(){return this.location+"/gloal_input_print_qr_2.mp4";},
    globalInputHome:function(){return this.location+"/global_input_home_3.mp4";},
 }
 export default config;
