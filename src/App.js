cd356de3605a15d97338652e3138a21ebcbc9297
Dilshat Hewzulla <hewzulla@gmail.com>
2019-03-13 15:20:21 +0000

    refactoring

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import "./css/loader.css";//removed after test
 import "./css/print.css";//removed after test
 import "./css/input.css";//removed after test
 import "./css/simpleAnimate.css";//removed after test
-import {pagelinks} from "./configs";
+import {pagelinks} from "./configs";//to be removed
 
 
-import {DevelopersScreen} from "./developers";
+import {DevelopersScreen} from "./developers"; //to be removed
 
 import screens from "./screens";
 
