6b87bdfff5ccd7017f738c2f671f4c32aa8f6673
Dilshat Hewzulla <hewzulla@gmail.com>
2019-03-17 00:40:53 +0000

    releasing

--- src/pages/contact-us-page/index.js
+++ src/pages/contact-us-page/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import HomePage from "../home-page";
+
 export default class ContactUsPage extends React.Component{
+  componentDidMount(){
+    this.scrollTo("contactUs")
+
+
+  }
+  componentDidUpdate(){
+    this.scrollTo("contactUs")
+  }
+
   render(){
     return(
-        <HomePage scrollTo="contactUs"/>
+        <HomePage/>
       );
   }
+
+  scrollTo(elementId){                        
+      var elmnt = document.getElementById(elementId);
+
+      if(elmnt){
+               window.scrollBy({top: -70,behavior: "smooth"});
+               elmnt.scrollIntoView();
+      }
+ }
 }
