c626d85035b94f4f86c06c07d31198c745f3ee96
Dilshat Hewzulla <hewzulla@gmail.com>
2019-04-02 20:52:06 +0100

    removed the service worker registation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import "babel-polyfill"
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as serviceWorker from './serviceWorker';
+
+
 
 import App from "./App";
 
-import registerServiceWorker from './registerServiceWorker';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
