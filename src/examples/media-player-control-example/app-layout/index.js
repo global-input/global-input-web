d6507acea4e5839e703c00dbc9aad48a7bee8917
Dilshat Hewzulla <hewzulla@gmail.com>
2020-02-19 09:51:58 +0000

    updated the media p;layer

--- src/examples/media-player-control-example/app-layout/index.js
+++ src/examples/media-player-control-example/app-layout/index.js
@@ -1,6 +1,21 @@
 import React, { useState, useLayoutEffect } from 'react';
 import { styles } from './styles';
 
+
+export function useWindowSize() {
+     const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+     useLayoutEffect(() => {
+          function updateSize() {
+               setSize([window.innerWidth, window.innerHeight]);
+          }
+          window.addEventListener('resize', updateSize);
+          updateSize();
+          return () => window.removeEventListener('resize', updateSize);
+     }, []);
+     return size;
+};
+
+
 export const QRCodeContainer = ({ children }) => {
      return (
           <div style={styles.barcodeContainer}>
@@ -11,9 +26,7 @@ export const QRCodeContainer = ({ children }) => {
      );
 }
 
-export const Title = ({ children }) => {
-     return (<div style={styles.title}>{children}</div>);
-}
+
 
 export const PageContainer = ({ children }) => {
      return (
@@ -30,19 +43,61 @@ export const PageContainer = ({ children }) => {
 export const VideoContainer = ({ children }) => {
      return (<div style={styles.videoContainer}>{children}</div>);
 }
+
+
+export const ErrorMessage= ({errorMessage})=>{
+     if(errorMessage){
+       return (<div style={styles.errorMessage.get()}>{errorMessage}</div>);
+     }
+     else {
+       return null;
+     }  
+ }
+
+ 
+
+
+
+
+export const Title = ({ children }) => {
+     return (<div style={styles.title}>{children}</div>);
+}
 export const P = ({ children }) => (<div style={styles.paragraph.get()}>{children}</div>);
 
 export const A = ({ href, children }) => (<a href={href} style={styles.link} target="__blank">{children}</a>);
 
-export function useWindowSize() {
-     const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
-     useLayoutEffect(() => {
-          function updateSize() {
-               setSize([window.innerWidth, window.innerHeight]);
-          }
-          window.addEventListener('resize', updateSize);
-          updateSize();
-          return () => window.removeEventListener('resize', updateSize);
-     }, []);
-     return size;
+export const Code=({children})=>(
+     <div style={styles.codeContainer}>
+         <pre style={styles.code}>{children}</pre>
+     </div>
+);
+
+export const Concept=({children})=>(<span style={styles.concept}>{children}</span>);
+
+
+export const TextAreaBox = ({value, id, onChange})=>(
+     <textarea  id={id} style={styles.form.textArea.get()}
+     onChange={onChange} value={value}/>            
+);
+
+export const TextInputBox=({id,type,readOnly,onChange,value,label})=>{
+     return(           
+              <div style={styles.form.row}>             
+                 <label htmlFor={id} style={styles.form.label}>{label}</label>
+                <input  id={id} style={styles.form.input}
+                
+               type={type}            
+               readOnly={readOnly}            
+               onChange={onChange} value={value}/>
+               
+             </div>
+         
+            
+     );
 };
+
+
+export const TextButton=({onClick,label})=>(
+     <button style={styles.form.textButton} onClick={onClick}>{label}</button>
+   );
+   
--- src/examples/media-player-control-example/app-layout/styles.js
+++ src/examples/media-player-control-example/app-layout/styles.js
@@ -97,4 +97,78 @@ export const styles = {
     color: "#6666ff"
   },
 
+  errorMessage:{
+    get:styleMatchingScreenSize,
+    default:{
+      fontSize:16,
+      display:"block",
+      marginBottom:20,
+      marginTop:20,
+      color:'red',
+    },
+    mobile:{
+      fontSize:16,
+    }
+  },
+
+
+  codeContainer:{
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"center",
+    width:"80%",
+    paddingLeft:10
+  },
+  code:{
+    backgroundColor:'rgba(169,200,230,0.1)',
+    overflow:'scroll',
+    width:"100%",
+    color:"#5291CD",
+  },
+
+  concept:{
+    borderBottomStyle:"dotted",
+    borderBottomColor:'black',
+    borderBottomWidth:1
+  },
+  
+  form:{
+          row:{
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"flex-start",
+            alignItems:"flex-start",
+            width:"90%"       
+          },
+          label:{
+            color:"#5291CD",
+            position:"relative",        
+            top:+10        
+          },
+          input:{
+              width:"100%"
+          },
+          textArea:{
+            get:()=>{
+              return{
+                width:"100%",
+                minHeight:200
+              }
+            }
+        
+          },
+          textButton:{
+            backgroundColor:"#5291CD",
+            borderRadius:5,
+            margin:"5vw",
+            color:"white",
+            padding:10,
+            minWidth:150,
+         },
+ }
+   
 };
+
+
+
+
