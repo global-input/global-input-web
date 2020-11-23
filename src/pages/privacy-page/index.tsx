2990657045e86866fba18367d747da389d28b409
Dilshat Hewzulla <hewzulla@gmail.com>
2020-11-23 01:28:29 +0000

    clean up code

--- src/pages/privacy-page/index.tsx
+++ src/pages/privacy-page/index.tsx
@@ -33,7 +33,7 @@ const PrivacyPage: React.FC = () => {
           A connected application can always request data on-demand via the mobile app, which will ask you to confirm to push the requested data over to the application. The communication between your devices is secured using end-to-end encryption and the encryption key that starts the session is dynamically generated within your device for each session and can only be accessed via an encrypted QR Code.
 </P>
         <P>
-          Your data in your mobole app always stays encrypted and decrypted only at the point of usage.  The keys used in the encryption never leave your mobile and are encrypted with a memorable password.  This means that you are responsible for backing up your encryption keys in your mobile app and the data that are encrypted with the encryption keys.  Since you can export an encryption key as encrypted QR code, and import it simply by scanning it,  the process of backing up the keys and sharing between your devices is a straightforward operation.
+          Your data in your mobile app always stays encrypted and decrypted only at the point of usage.  The keys used in the encryption never leave your mobile and are encrypted with a memorable password.  This means that you are responsible for backing up your encryption keys in your mobile app and the data that are encrypted with the encryption keys.  Since you can export an encryption key as encrypted QR code, and import it simply by scanning it,  the process of backing up the keys and sharing between your devices is a straightforward operation.
 </P>
 
 
