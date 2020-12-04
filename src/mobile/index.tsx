6fd17875110a143a3ce539a7f62aeab1532fe914
Dilshat Hewzulla <hewzulla@gmail.com>
2020-12-04 10:24:35 +0000

    removed-unused

--- src/mobile/index.tsx
+++ src/mobile/index.tsx
@@ -2,22 +2,3 @@
 import * as globalInput from 'global-input-react';////global-input-react////
 export * from './useMobile';
 export * from './useConnectToMobile';
-
-
-export const userWithDomainAsFormId = (initData: globalInput.InitData) => {
-    if (initData?.form?.domain && initData?.form?.fields?.length) {
-        const textFields = initData.form.fields.filter(f => {
-            if ((!f.type) || f.type === 'text') {
-                if (f.nLines && f.nLines > 1) {
-                    return false;
-                }
-                return true;
-            }
-            return false;
-        });
-        if (!textFields.length) {
-            return null;
-        }
-        initData.form.id = `###${textFields[0].id}###@${initData.form.domain}`;
-    }
-};
