9497036daccee36c1be9a03d6a537df64bbe14fb
Dilshat Hewzulla <hewzulla@gmail.com>
2020-12-06 20:10:25 +0000

    top-menu-styled-components

--- src/page-components/top-header-section/index.tsx
+++ src/page-components/top-header-section/index.tsx
@@ -1,19 +1,134 @@
-
-import React, { useState, useEffect, useCallback } from 'react';
-import { Link } from 'react-router-dom'
-import { screenMedia, styleMatchingScreenSize } from "../../app-layout/screen-media";
-import useWindowSize from '../../app-layout/use-window-size';
-
-
+import React, { useState, useEffect,useRef} from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { config } from "../../configs";
 import appIcon from './images/app-icon.png';
-import companyIcon from './images/company-icon.png';
 import menuSymbol from './images/menu-symbol.svg';
 
-const appTitle = config.id === 'iterative' ? 'Iterative Solution Limited' : 'Global Input App';
-const appLogo = config.id === 'iterative' ? companyIcon : appIcon;
+interface Props {
+  selected?: string | null;
+}
+export const TopHeaderSection: React.FC<Props> = ({ selected }) => {
+  const [menuPressed, setMenuPressed] = useState(false);
 
-var menus = [{
+  const mobileMenuContainer=useRef(null);
+  useClickedOutside(mobileMenuContainer, ()=>{
+    if(menuPressed){
+      setMenuPressed(false);
+    }
+  });
+  const toggle = () => setMenuPressed(menuPressed => !menuPressed);
+
+  const listMenu=(menus.map((menu, index) => (
+    <MenuItem to={menu.link}
+        selected={menu.link === selected}
+        key={`${index}_${menu.link}_${menu.linkText}`}>
+        { menu.linkText}
+   </MenuItem >
+  )));
+
+  return (
+    <Container>
+      <TopBar>
+          <Icon onClick={toggle}>
+                <CloseIcon show={menuPressed}>X</CloseIcon>
+                <OpenIcon show={!menuPressed}/>
+          </Icon>
+          <Logo />
+          <AppTitle />
+        <Desktop>
+              {listMenu}
+        </Desktop>
+      </TopBar>
+      {menuPressed && (<Mobile ref={mobileMenuContainer}>
+              {listMenu}
+      </Mobile>)}
+    </Container>
+  );
+};
+
+
+
+
+
+
+const Title = styled.div`
+    font-size: 20;
+    color: #5291CD;
+    white-space: nowrap;
+    font-weight: 300;
+    padding-left: 10px;
+    font-family: Tisa-Sans-Pro, Elysio-Light, Helvetica, Arial, sans-serif;
+`;
+const appTitle = config.id === 'iterative' ? 'Iterative Solution Limited' : 'Global Input App';
+export const AppTitle: React.FC = () => (
+  <Title>{appTitle}</Title>
+);
+
+
+export const Logo = styled.img.attrs({
+  alt: 'App Logo',
+  src: appIcon,
+})`
+  max-width: 80px;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 4px;
+  margin-top: 4px;
+  `;
+
+
+
+const Container = styled.div`
+      display: flex;
+      @media print{
+        display:none;
+      }
+      flex-direction: column;
+      width: 100%;
+      justify-content: flex-start;
+      align-items: flex-start;
+      position: fixed;
+      z-index: 100;
+      top: 0;
+      left: 0;
+`;
+
+const TopBar = styled.div`
+      padding-right: 30px;
+      position: static;
+      padding-top: 10px;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      padding-top: 5px;
+      padding-right: 50px;
+      border-bottom-color: #EEEEEE;
+      border-bottom-style: solid;
+      border-bottom-width: 1px;
+      box-shadow: 0 -5px 5px -5px #333;
+      background-color:white;
+`;
+
+
+
+const useClickedOutside = (element, onClicked) => {
+  useEffect(() => {
+      const handleClick = (evt) => {
+          if (element.current && (!element.current.contains(evt.target))) {
+              onClicked();
+          }
+      }
+      document.addEventListener('click', handleClick);
+      return () => {
+          document.removeEventListener('click', handleClick);
+      }
+  }, [onClicked, element]);
+};
+
+const menus = [{
   link: config.paths.home.path,
   linkText: "HOME"
 }, {
@@ -26,259 +141,100 @@ var menus = [{
   link: config.paths.getAppScreen.path,
   linkText: "GET IT FREE"
 }];
-
-interface Props {
-  selected?: string | null;
-}
-
-interface Menu {
-  link: string;
-  linkText: string;
-}
-
-const getMenuItemStyle = (hover, isSelected) => {
-  if (hover) return styles.menuItem.get('hover');
-  if (isSelected) return styles.menuItem.get('selected');
-  return styles.menuItem.get();
-};
-
-
-export const TopHeaderSection: React.FC<Props> = ({ selected }) => {
-  const [menuPressed, setMenuPressed] = useState(false);
-  const [hover, setHover] = useState<Menu | null>(null);
-  const bgs = screenMedia.biggerThan(600);
-  const containerStyle = bgs ? styles.topNavContainer.desktop : styles.topNavContainer.mobile
-  const navStyle = bgs ? styles.topNav.desktop : styles.topNav.mobile
-  const titleStyle = bgs ? styles.appDesktopTitle : styles.appMobileTitle;
-  const [width] = useWindowSize();
-
-
-  console.log("render menu:" + width);
-  useEffect(() => {
-    setMenuPressed(false);
-  }, [width]);
-
-  const hideMenu = () => setMenuPressed(false);
-  const renderedMenu = menus.map((menu, index) => (
-    <Link to={menu.link} style={getMenuItemStyle(hover === menu, selected && menu.link === selected)} key={`${index}_${menu.link}_${menu.linkText}`}
-      onMouseEnter={() => setHover(menu)} onMouseLeave={() => setHover(null)} data-testid="top-menu-item">{menu.linkText}</Link>));
+const MenuItem = styled(Link)`
+      float: left;
+      display: block;
+      text-align: center;
+      padding: 8px 16px;
+      text-decoration: none;
+      font-size: 15px;
+      font-weight: ${props => props.selected ? 500 : 300};
+      margin-bottom: 0;
+      white-space: nowrap;
+      &:hover{
+        color: "#66ccff",
+        fontWeight: 300,
+        textDecoration: "underline"
+      }
+      @media (min-width: 601px){
+        border-top-right-radius: 10px;
+        border-top-left-radius: 10px;
+        margin-bottom: 10px;
+        font-size: 12px;
+        color: #5291CD;
+      }
+      @media (min-width: 800px){
+        font-size: 15px;
+      }
+      @media (max-width: 599px){
+        text-align: left;
+        background-color: #FFFFFF;
+        border-bottom-color: #BBBBBB;
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+      }
+`;
 
 
 
-  return (
-    <div style={containerStyle} className="noprint">
-      <div style={navStyle}>
-        {(!bgs) && (
-          <div style={styles.mobileMenu}>
-            <a style={styles.mobileMenuIcon} href="#b" data-testid="mobile-to-close-menu" onClick={() => {
-              setMenuPressed(!menuPressed);
-            }}>
-              {menuPressed ? '☓' : (<img src={menuSymbol} alt="Menu" />)}
-            </a>
 
-          </div>
-        )}
-        <img src={appLogo} style={styles.logo} alt="App Logo" />
-        <div style={styles.appTitleContainer}>
-          <div style={titleStyle}>{appTitle}</div>
-        </div>
-        {bgs && (
-          <div style={styles.menuItemsDesktop}>
-            {renderedMenu}
-          </div>
-        )}
-      </div>
+export const listMenus = ({ selected }) => menus.map((menu, index) => (
+                <MenuItem to={menu.link}
+                    selected={menu.link === selected}
+                    key={`${index}_${menu.link}_${menu.linkText}`}>
+                    { menu.linkText}
+                </MenuItem >));
 
-      {(!bgs) && menuPressed && (
-        <div style={styles.menuItemsMobile}>
-          {renderedMenu}
-          <a onClick={hideMenu} href="#b" >
-            <div style={styles.mobileMenuOverlay}>
-            </div>
-          </a>
-        </div>
-      )}
-    </div>
-  );
-};
-const fontFamily = "Tisa-Sans-Pro, Elysio-Light, Helvetica, Arial, sans-serif";
-export const styles = {
-  topNavContainer: {
-    desktop: {
-      display: "flex",
-      flexDirection: "column" as 'column',
-      width: "100%",
-      justifyContent: "flex-start",
-      alignItems: "flex-start",
-      position: "fixed" as 'fixed',
-      zIndex: 100,
-      top: 0,
-      left: 0,
-      paddingTop: 5,
-      paddingRight: 50,
-      borderBottomColor: '#EEEEEE',
-      borderBottomStyle: "solid" as 'solid',
-      borderBottomWidth: 1,
-      boxShadow: "0 -5px 5px -5px #333",
-      backgroundColor: "white",
-    },
-    mobile: {
-      display: "flex",
-      flexDirection: "column" as 'column',
-      width: "100%",
-      justifyContent: "flex-start",
-      alignItems: "flex-start",
-      position: "fixed" as 'fixed',
-      zIndex: 100,
-      top: 0,
-      left: 0
+const Icon = styled.button`
+    font-weight: normal;
+    color: #5291CD;
+    font-style: normal;
+    text-decoration: none;
+    border: none;
+    background-color:white;
+    min-width:50px;
+    &: focus {
+        outline:0;
     }
-
-  },
-
-  topNav: {
-    desktop: {
-      paddingRight: 30,
-      position: "static" as 'static',
-      paddingTop: 10,
-      width: "100%",
-      display: "flex",
-      flexDirection: "row" as 'row',
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    mobile: {
-      paddingRight: 30,
-      paddingTop: 10,
-      width: "100%",
-      display: "flex",
-      flexDirection: "row" as 'row',
-      justifyContent: "space-between",
-      alignItems: "center",
-      backgroundColor: "white",
-      borderBottomColor: '#EEEEEE',
-      borderBottomStyle: "solid" as 'solid',
-      borderBottomWidth: 2,
-
-
+    display:none;
+    @media only screen and (max-width: 599px) {
+      display:block;
     }
-
-  },
-  appTitleContainer: {
-    marginLeft: 0
-  },
-
-  appDesktopTitle: {
-    fontSize: 20,
-    color: "#5291CD",
-    whiteSpace: "nowrap" as 'nowrap',
-    fontWeight: 300,
-    paddingLeft: 10,
-    fontFamily: fontFamily
-  },
-  appMobileTitle: {
-    fontSize: 20,
-    color: "#5291CD",
-    whiteSpace: "nowrap" as 'nowrap',
-    fontWeight: 300,
-    fontFamily: fontFamily,
-
-  },
-  appVersion: {
-    fontFamily: fontFamily,
-    fontSize: 12,
-    color: "#5291CD",
-    marginLeft: 10
-  },
-
-  mobileMenu: {
-
-    marginLeft: 20
-  },
-  mobileMenuIcon: {
-    fontWeight: 'normal' as 'normal',
-    fontSize: 40,
-    color: "#5291CD",
-    fontStyle: 'normal',
-  },
-  mobileMenuOverlay: {
-    width: "100%",
-    height: 1000
-  },
-  menuItemsDesktop: {
-    display: "flex",
-    flexDirection: "row" as 'row',
-    right: 0,
-    top: 24,
-    width: "100%",
-    justifyContent: "flex-end",
-    paddingRight: "10vw"
-  },
-  menuItemsMobile: {
-    display: "flex",
-    flexDirection: "column" as 'column',
-    backgroundColor: "rgba(0,0,0,0)",
-    width: "100%",
+`;
+const CloseIcon = styled.div`
+    font-size:24px;
+    display:${props=>props.show?'block':'none'};
+`;
+const OpenIcon = styled.img.attrs({
+    alt:'Menu',
+    src:menuSymbol,
+    width:22,
+    height:20
+})`
+display:${props=>props.show?'block':'none'};
+`;
 
 
-  },
 
-
-  menuItem: {
-    get: styleMatchingScreenSize,
-    default: {
-      float: "left",
-      display: "block",
-      textAlign: "center",
-      padding: "8px 16px",
-      textDecoration: "none",
-      fontSize: 15,
-      marginBottom: 0,
-      color: "#5291CD",
-      whiteSpace: "nowrap",
-      fontWeight: 300
-    },
-    desktop: {
-      borderTopRightRadius: 10,
-      borderTopLeftRadius: 10,
-      marginBottom: 10,
-      fontSize: 12,
-      color: "#5291CD"
-    },
-    smallScreen: {
-      fontSize: 15,
-    },
-
-    mobile: {
-      textAlign: "left",
-      backgroundColor: "#FFFFFF",
-      borderBottomColor: '#BBBBBB',
-      borderBottomStyle: "solid",
-      borderBottomWidth: 1
-    },
-    selected: {
-      color: "#5291CD",
-      fontWeight: 500
-    },
-    hover: {
-      color: "#66ccff",
-      fontWeight: 300,
-      textDecoration: "underline"
-    }
-  },
-  content: {
-    position: "absolute",
-    marginTop: 90,
-    width: "100%",
-    padding: 20
-  },
-
-  logo: {
-    maxWidth: 80,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 4,
-    marginTop: 4
+const Desktop = styled.div`
+  flex-direction:row;
+  margin:0;
+  padding:0;
+  display:none;
+  @media only screen and (min-width: 600px){
+    display:flex;
   }
+`;
 
-};
+const Mobile = styled.div`
+    display: none;
+    flex-direction: column;
+    right: 0;
+    top: 24px;
+    justify-content: flex-end;
+    padding-right: 10vw;
+    @media (max-width: 599px){
+        display: flex;
+        background-color: rgba(0,0,0,0);
+    }
+`;
