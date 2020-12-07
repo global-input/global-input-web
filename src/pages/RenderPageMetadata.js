883f653e819b59d7fce6c73b972ec6abdafe5bd0
Dilshat Hewzulla <hewzulla@gmail.com>
2020-12-07 18:03:19 +0000

    removed-unused

--- src/pages/RenderPageMetadata.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Helmet} from 'react-helmet'
-export default ({title="Global Input App - Introducing Mobile Interoperability to Web and Device Applications"})=>(
-  <Helmet>
-        <title>{title}</title>
-  </Helmet>
-);
