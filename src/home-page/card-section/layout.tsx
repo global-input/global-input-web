bc50b588cfdbf912b322b0fdfd50415170e57ae8
Dilshat Hewzulla <hewzulla@gmail.com>
2020-12-21 23:11:30 +0000

    card-scroll

--- src/home-page/card-section/layout.tsx
+++ src/home-page/card-section/layout.tsx
@@ -73,6 +73,8 @@ export const Card =styled (Link)`
       padding-bottom:30px;
       max-width: 370px;
       text-decoration:none;
+      overflow:scroll;
+
       &: hover{
         box-shadow: 0 0 50px #ffff;
         transform: translateY(-3px);
