b21618f88f9d1382118ced4b3054174638f11403
luca adams <lucaads2006@gmail.com>
2022-07-08 10:04:05 +0100

    add space below the cards

--- src/home-page/card-section/layout.tsx
+++ src/home-page/card-section/layout.tsx
@@ -135,6 +135,7 @@ export const Cards=styled.div`
    justify-content:flex-start;
    align-items:center;
    width:100%;
+   height: 100vh;
 `;
 export const CardsContent=styled.div`
    display: flex;
