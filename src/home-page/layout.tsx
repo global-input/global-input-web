e7e7b7fcb096b53d37391a326f3f614b55a32d58
luca adams <lucaads2006@gmail.com>
2022-07-08 09:44:46 +0100

    remove horizontal scroll bar on homepage

--- src/home-page/layout.tsx
+++ src/home-page/layout.tsx
@@ -24,7 +24,7 @@ export const Container = styled.div`
 `;
 
 export const Content=styled.div`
-    width: 100vw;
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
