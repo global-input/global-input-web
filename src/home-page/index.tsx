8b5a73103a205c01e79047761dcea5d9585a126c
Dilshat Hewzulla <hewzulla@gmail.com>
2022-07-08 18:02:59 +0100

    added back disconnect buttom

--- src/home-page/index.tsx
+++ src/home-page/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { config } from '../configs';
 import {PageFooter} from '../page-footer';
 import {PageHeader} from '../page-header';
-import { useConnectToMobile,ConnectWindow,ConnectButton } from './mobile-ui';
+import { useConnectToMobile,ConnectWindow,ConnectButton,DisconnectButton } from './mobile-ui';
 import { usePageTitle } from '../page-metadata';
 import {PosterImage} from './poster-image';
 import {HowItWorks} from "./how-it-works";
@@ -36,6 +36,7 @@ export const HomePage: React.FC<HomePageProps> = () => {
                 <ButtonContainer>
                   <LinkButton to={config.paths.getAppScreen.path}>Get it free</LinkButton>
                   <ConnectButton label="Try it out" mobile={mobile} />
+                  <DisconnectButton mobile={mobile} skin='white'/>
                 </ButtonContainer>
           </TitleSection>
 
