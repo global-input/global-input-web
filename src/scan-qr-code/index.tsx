adcefec2298ca4a81a218ce3250cb59e3858ff78
Dilshat Hewzulla <hewzulla@gmail.com>
2024-11-04 17:31:00 +0000

    scan-qr-code

--- src/scan-qr-code/index.tsx
+++ src/scan-qr-code/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import Switch from "react-switch";
 import { PageHeader } from "../page-header";
 import { PageFooter } from "../page-footer";
-import { QrReader } from '@blackbox-vision/react-qr-reader';
+import { Scanner, IDetectedBarcode } from '@yudiel/react-qr-scanner';
 import DisplayUserLogin from "../display-user-login";
 import DisplayCode from "./DisplayCode";
 import {
@@ -32,13 +32,9 @@ const StartScanQRCode: React.FC = () => {
   
   usePageTitle("Global Input App - Scan QR Code");
 
-  const setContentAndMessage = useCallback((content, message) =>{         
-    setData (data => ({...data, content, message}));    
-  },[]);
   
-
   const onDataInputSwitchChange =useCallback((inputActive) => {
-    console.log("-----:inputActive"+ inputActive);
+    
     setData(data=>{
         return {...data, inputActive: !!inputActive};        
     });
@@ -46,76 +42,48 @@ const StartScanQRCode: React.FC = () => {
   },[]);
 
 
-  const handleScan = useCallback((result, error) => {
-    if (error) {
-      switch(error.name){
-        case 'NotAllowedError':
-          console.log("---Permission denied");
-          setContentAndMessage('', 'Permission denied');
-          break;
-        case 'NotReadableError':
-          console.log("---Cannot read the image");
-          setContentAndMessage('', 'Cannot read the image');
-          break;
-        case 'NotFoundException':                    
-          break;
-        default:
-          console.log("---Error:=------"+error.message, error);
-          setContentAndMessage('', error.name);          
-        }        
-        return;      
-    }
-    if(!result){
-      console.log("result is empty");
-      return;
-    }
-    var currentTime = new Date().getTime()
-    if (lastCodeDataProcessed) {      
-      if ((currentTime - lastCodeDataProcessed.lastTime) < 2000) {
-        
-          return
-      }      
-    } 
-    else{
-      lastCodeDataProcessed={};
-      console.log("first time");
-    }
-    console.log("result="+result.getText());
-    lastCodeDataProcessed.lastTime=currentTime;
-    lastCodeDataProcessed.code=result?.getText();
-    if(navigator.vibrate){
-        navigator.vibrate(200);
-    }
-    setData(data=>{
-      if (!data.inputActive) { 
-        console.log("------:lastCodeDataProcessed.code"+ lastCodeDataProcessed.code);
 
-        return {...data, content:lastCodeDataProcessed.code, message:'QR Code Content'};        
-      }
-      if (appdata.isActiveEncryptionKeyEncryptedMessage(lastCodeDataProcessed.code)) {
-        const decryptedContent = appdata.decryptCodeDataWithAnyEncryptionKey(lastCodeDataProcessed.code);
-        console.log("------:decryptedContent"+ decryptedContent);
-        if (decryptedContent) {
-          
-          return {...data, content:decryptedContent, message:'Decrypted QR Code Content'};        
-
-        } else {
-          
-          return {...data, content:lastCodeDataProcessed.code, message:'Failed to decrypt the content!'};        
-
-        }
-      }
-
-      return data;
-
-    });
+const onScanCodes = useCallback((code: IDetectedBarcode[]) => {
     
+    if (code.length > 0) {
+      for(let i=0;i<code.length;i++){                
+            setData(data=>{
+              
+
+              if(!code[i].rawValue){
+                return data;
+              }
+              
+              
+              if (!data.inputActive) {                     
+                return {...data, content:code[i].rawValue, message:'QR Code Content'};        
+              }
+              if (appdata.isActiveEncryptionKeyEncryptedMessage(code[i].rawValue)) {
+                const decryptedContent = appdata.decryptCodeDataWithAnyEncryptionKey(code[i].rawValue);      
+                if (decryptedContent) {
+                  
+                  return {...data, content:decryptedContent, message:'Decrypted QR Code Content'};        
+          
+                } else {
+                  
+                  return {...data, content:code[i].rawValue, message:'Failed to decrypt the content!'};        
+          
+                }
+              }
+          
+              return data;
+          
+            });
+          
+      }
+    }
+
     
 
-  }, [setContentAndMessage]);
+  },[]);
     
   
-console.log("****data.inputActive="+data.inputActive);
+
   return (
     <Container>
       <PageHeader selected={config.paths.contactUs.path} />
@@ -129,11 +97,16 @@ console.log("****data.inputActive="+data.inputActive);
         
         
         
-      <QrReader
-        onResult={handleScan}
-        constraints={{ facingMode: 'environment' }}
-        containerStyle={{ width: '100%' }}
-        videoStyle={{ width: '100%' }}
+      <Scanner
+        onScan={onScanCodes}
+        allowMultiple={true}
+        scanDelay={1000}
+
+        
+        
+        // constraints={{ facingMode: 'environment' }}
+        // containerStyle={{ width: '100%' }}
+        // videoStyle={{ width: '100%' }}
       />
       <DisplayCode  title={data.message} content={data.content}/>
       
