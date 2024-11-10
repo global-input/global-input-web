0a329266ced25775a4153681b901544dfd964295
Dilshat Hewzulla <hewzulla@gmail.com>
2024-11-10 11:29:44 +0000

    mock-qr-code

--- src/__mocks__/@yudiel/react-qr-scanner.tsx
+++ src/__mocks__/@yudiel/react-qr-scanner.tsx
@@ -4,17 +4,26 @@ import { IScannerProps, IDetectedBarcode } from '@yudiel/react-qr-scanner';
 // Mock Scanner Component
 const MockScanner: React.FC<IScannerProps> = ({ onScan }) => {
   useEffect(() => {
+    let scanTimeout: NodeJS.Timeout;
+
     // Simulate QR code scan by calling onScan with mock data after a short delay
-    const mockBarcodeData: IDetectedBarcode = {
-      boundingBox: { x: 0, y: 0, width: 100, height: 100 },
-      cornerPoints: [{ x: 0, y: 0 }, { x: 100, y: 0 }, { x: 100, y: 100 }, { x: 0, y: 100 }],
-      format: 'QR_CODE',
-      rawValue: 'AU2FsdGVkX18gypzRU7BWxeVidh9NLq5mNe8JkMfm8CxOitHHQHnbRuzyiVQRKUBaV9tALiKlbXS6rcmcR5a2aCTxnSkTmvydX5TbzALeH6JH2my4he/dKYCqWYxk9Yw52gjnKm3Bp1BcmzYtt9ANNAIdMnx8Vtc5OB4tUZDo9Bx6kl4VMaMia3cImK2b70Y5LpmmvNqiDS+QMKQGPhtd0bl64p+mujDNbNGREfWD4RI%3D', // Replace with the specific code you want to simulate
+    const startMock= async () => {
+      const codeContent= await fetch('/test/connect.txt')
+      const codeString= await codeContent.text()          
+      console.log("-----mocking qr code: ", codeString);
+      await new Promise(resolve => setTimeout(resolve, 500));      
+      const mockBarcodeData: IDetectedBarcode = {
+        boundingBox: { x: 0, y: 0, width: 100, height: 100 },
+        cornerPoints: [{ x: 0, y: 0 }, { x: 100, y: 0 }, { x: 100, y: 100 }, { x: 0, y: 100 }],
+        format: 'QR_CODE',
+        rawValue: codeString, // Replace with the specific code you want to simulate
+      };        
+      scanTimeout = setTimeout(() => {
+        onScan([mockBarcodeData]);
+      }, 500); // Delay in milliseconds
     };
 
-    const scanTimeout = setTimeout(() => {
-      onScan([mockBarcodeData]);
-    }, 500); // Delay in milliseconds
+    startMock();
 
     return () => clearTimeout(scanTimeout);
   }, [onScan]);
