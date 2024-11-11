dd32854012d3da29f38f079bd8412573af1327ed
Dilshat Hewzulla <hewzulla@gmail.com>
2024-11-11 09:18:24 +0000

    suspense-to-mobile-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,Suspense } from "react";
 import { useLocation } from "react-router-dom";
 import {
   BrowserRouter as Router,
@@ -98,7 +98,11 @@ const App: React.FC = () => (
         path={config.paths.examples.mobileEncryption.oldPath}
         element={<microApps.OldMobileEncryptionScreen/>}
       />
-      <Route path="/global-input-app/mobile-app" element={<MobileApp/>} />
+      <Route path="/global-input-app/mobile-app" element={
+      <Suspense fallback={<LoadingCircle/>}>
+        <MobileApp/>
+      </Suspense>
+      } />
       
 
       
@@ -111,3 +115,22 @@ const App: React.FC = () => (
 );
 
 export default App;
+
+const styles = {
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+};
+const LoadingCircle = () => (
+  <div style={styles.loading}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+          <path fill="#C779D0" d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z">
+              <animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.5s" repeatCount="indefinite" />
+          </path>
+      </svg>
+  </div>
+);
+
