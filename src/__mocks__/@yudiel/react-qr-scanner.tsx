229c1247aa2808d44413dbd400d1d8623a2c60a6
Dilshat Hewzulla <hewzulla@gmail.com>
2024-11-13 10:33:42 +0000

    test-import-key

--- src/__mocks__/@yudiel/react-qr-scanner.tsx
+++ src/__mocks__/@yudiel/react-qr-scanner.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { IScannerProps, IDetectedBarcode } from '@yudiel/react-qr-scanner';
 
+const test_code= '/test/import_key.txt';
+
 // Mock Scanner Component
 const MockScanner: React.FC<IScannerProps> = ({ onScan }) => {
   useEffect(() => {
@@ -8,19 +10,23 @@ const MockScanner: React.FC<IScannerProps> = ({ onScan }) => {
 
     // Simulate QR code scan by calling onScan with mock data after a short delay
     const startMock= async () => {
-      const codeContent= await fetch('/test/connect.txt')
-      const codeString= await codeContent.text()          
-      console.log("-----mocking qr code: ", codeString);
-      await new Promise(resolve => setTimeout(resolve, 500));      
-      const mockBarcodeData: IDetectedBarcode = {
-        boundingBox: { x: 0, y: 0, width: 100, height: 100 },
-        cornerPoints: [{ x: 0, y: 0 }, { x: 100, y: 0 }, { x: 100, y: 100 }, { x: 0, y: 100 }],
-        format: 'QR_CODE',
-        rawValue: codeString, // Replace with the specific code you want to simulate
-      };        
-      scanTimeout = setTimeout(() => {
-        onScan([mockBarcodeData]);
-      }, 500); // Delay in milliseconds
+      try{
+          const codeContent= await fetch(test_code)
+          const codeString= await codeContent.text()          
+          console.log("-----mocking qr code: ", codeString);
+          await new Promise(resolve => setTimeout(resolve, 500));      
+          const mockBarcodeData: IDetectedBarcode = {
+            boundingBox: { x: 0, y: 0, width: 100, height: 100 },
+            cornerPoints: [{ x: 0, y: 0 }, { x: 100, y: 0 }, { x: 100, y: 100 }, { x: 0, y: 100 }],
+            format: 'QR_CODE',
+            rawValue: codeString, // Replace with the specific code you want to simulate
+          };              
+          onScan([mockBarcodeData]);
+        }
+        catch(error){
+          console.error("Error in MockScanner: ", error);
+        }
+      
     };
 
     startMock();
