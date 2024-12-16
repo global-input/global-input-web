9f3d619b058536f9f332020625700001ec9858ae
Dilshat Hewzulla <hewzulla@gmail.com>
2024-12-16 11:42:24 +0000

    test-prepare

--- src/mobile/widgets.tsx
+++ src/mobile/widgets.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { ConnectQR, PairingQR } from "global-input-react"; ////global-input-react////
+import { ConnectQR, PairingQR } from 'global-input-react'; ////global-input-react////
 
 import { WidgetState, MobileData } from "./commons";
 
