85f779d0353e42a1992ba960d88de3eb901bdc0f
Dilshat Hewzulla <hewzulla@gmail.com>
2024-12-30 18:49:54 +0000

    warning

--- src/micro-apps/mobile-encryption-example/common-elements/copyButton.tsx
+++ src/micro-apps/mobile-encryption-example/common-elements/copyButton.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 
 interface CopyElementProps{
-    show:boolean;
+    $show:boolean;
     position?:number;
     children?: React.ReactNode;
     
@@ -17,10 +17,10 @@ const CopyContainer1=styled.div<CopyElementProps>`
     align-self:flex-end;
     align-items:center;
     top:-16px;
-    background-color: ${props=>props.show?'white':''};
+    background-color: ${props=>props.$show?'white':''};
     z-Index:5;
     padding-left:5px;
-    border:${props=>props.show?'1px dotted #4040bf':''};
+    border:${props=>props.$show?'1px dotted #4040bf':''};
 `;
 const CopyContainer2=styled(CopyContainer1)`
 
@@ -31,21 +31,21 @@ const CopyContainer3=styled.div<CopyElementProps>`
     flex-direction:row;
     position:relative;
     align-items:flex-start;
-    background-color: ${props=>props.show?'white':''};
+    background-color: ${props=>props.$show?'white':''};
     z-Index:5;
 
 
 `;
 
-const CopyContainer:React.FC<CopyElementProps>=({position=1,children,show})=>{
+const CopyContainer:React.FC<CopyElementProps>=({position=1,children,$show})=>{
     if(position===2){
-        return (<CopyContainer2 show={show}>{children}</CopyContainer2>);
+        return (<CopyContainer2 $show={$show}>{children}</CopyContainer2>);
     }
     else if(position===3){
-        return (<CopyContainer3 show={show}>{children}</CopyContainer3>);
+        return (<CopyContainer3 $show={$show}>{children}</CopyContainer3>);
     }
     else{
-        return (<CopyContainer1 show={show}>{children}</CopyContainer1>);
+        return (<CopyContainer1 $show={$show}>{children}</CopyContainer1>);
     }
 }
 
@@ -55,7 +55,7 @@ const CopyContent=styled.div<CopyElementProps>`
     white-space: wrap;
     font-size: 12px;
     padding-right:10px;
-    display:${props=>props.show?'block':'none'};
+    display:${props=>props.$show?'block':'none'};
 `;
 
 const Button = styled.button<CopyElementProps>`
@@ -80,7 +80,7 @@ const Button = styled.button<CopyElementProps>`
         box-shadow: 0 0 50px #ffff;
     }
     display:flex;
-    visibility:${props=>props.show?'visible':'hidden'};
+    visibility:${props=>props.$show?'visible':'hidden'};
 
 `;
 
@@ -113,11 +113,11 @@ export const CopyToClipboardButton:React.FC<Props>=({children,value,position=1})
     }, 2000);
   }
     return (
-    <CopyContainer show={copying} position={position}>
-            <CopyContent show={copying}>
+    <CopyContainer $show={copying} position={position}>
+            <CopyContent $show={copying}>
                 copied into your clipboard
             </CopyContent>
-            <Button onClick={onCopy} show={(!copying) && !!navigator.clipboard && !!value}>Copy</Button>
+            <Button onClick={onCopy} $show={(!copying) && !!navigator.clipboard && !!value}>Copy</Button>
 
     </CopyContainer>
     );
